6118860f248e80813f76019c079aa7847eed1c77
moir_shota <mori.shota.q@alhinc.jp>
2025-06-10 13:40:24 +0900

    【entity.Form.UserService.UserRepositoryの追加】

--- src/main/java/com/example/Morihara/controller/LoginController.java
+++ src/main/java/com/example/Morihara/controller/LoginController.java
@@ -1,12 +1,23 @@
 package com.example.Morihara.controller;
 
 import com.example.Morihara.controller.Form.UserForm;
+import com.example.Morihara.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
+    @Autowired
+    UserService userService;
+
     @GetMapping
     public ModelAndView login() {
         ModelAndView mav = new ModelAndView();
@@ -18,5 +29,23 @@ public class LoginController {
         mav.addObject("formModel", userForm);
         // mav.addObject("errorMessageForm", errorMessages);
         return mav;
+    }
+
+  @PostMapping("/loading")
+    public ModelAndView addContent(
+            @Valid @ModelAttribute("formModel") UserForm userForm,
+            BindingResult result,
+            RedirectAttributes redirectAttributes,
+            Model model){
+        if (result.hasErrors()) {
+            ModelAndView mav = new ModelAndView("/login");
+            mav.addObject("formModel", userForm);
+            return mav;
+        }
+
+        // 投稿をテーブルに格納
+        userService.findByAccountAndPassword(userForm);
+        // rootへリダイレクト
+        return new ModelAndView("redirect:/home");
     }
 }
--- src/main/java/com/example/Morihara/service/UserService.java
+++ src/main/java/com/example/Morihara/service/UserService.java
@@ -1,4 +1,60 @@
 package com.example.Morihara.service;
 
+import com.example.Morihara.controller.Form.UserForm;
+import com.example.Morihara.repository.UserRepository;
+import com.example.Morihara.repository.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    @Autowired
+    private final UserRepository userRepository;
+    PasswordEncoder passwordEncoder;
+
+    public UserForm findByAccountAndPassword(UserForm userForm){
+
+        List<User> results = new ArrayList<>();
+        String account = userForm.getAccount();
+        String password = createEndocedPwd(userForm.getPassword());
+        results.add((User) userRepository.findByAccountAndPassword(account, userForm.getPassword()));
+        List<UserForm> users = setUserForm(results);
+        return users.get(0);
+    }
+
+    private List<UserForm> setUserForm(List<User> results) {
+        List<UserForm> users = new ArrayList<>();
+
+        for (int i = 0; i < results.size(); i++) {
+            UserForm user = new UserForm();
+            User result = results.get(i);
+            user.setId(result.getId());
+            user.setAccount(result.getAccount());
+            user.setPassword(result.getPassword());
+            user.setBranchId(result.getBranchId());
+            user.setDepartmentId(result.getDepartmentId());
+            user.setCreatedDate(result.getCreatedDate());
+            user.setUpdatedDate(result.getUpdatedDate());
+            users.add(user);
+        }
+        return users;
+    }
+        private String createEndocedPwd(String pwd) {
+           String encodedPwd = passwordEncoder.encode(pwd);
+        return encodedPwd;
+    }
+
+    // 暗号化されたあとのpw同士を比較する
+    private boolean pwdMatch(String newPwd, String originPwd) {
+        if (passwordEncoder.matches(newPwd, originPwd)) {
+            return true;
+        }
+        return false;
+    }
 }
