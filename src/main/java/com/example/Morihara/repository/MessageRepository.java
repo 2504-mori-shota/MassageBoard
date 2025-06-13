db0ad0b5d488db2fcf12f5c329eae9b5518916f6
moir_shota <mori.shota.q@alhinc.jp>
2025-06-13 15:58:17 +0900

    【絞り込み機能実装中】

--- src/main/java/com/example/Morihara/repository/MessageRepository.java
+++ src/main/java/com/example/Morihara/repository/MessageRepository.java
@@ -5,8 +5,10 @@ import com.example.Morihara.repository.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
+    List<Message>findByCreteDDateBetweenOrderByCreatedDateAsc(Date StrDate, Date EndDate);
 }
