f4533c4ef517a376bf6c5ad91974012f61ffcd5d
iwahara_aoi <iwahara.aoi@alhinc.jp>
2025-06-13 11:13:22 +0900

    ログインフィルター作成

---
+++ src/main/java/com/example/Morihara/config/FilterConfig.java
@@ -0,0 +1,22 @@
+package com.example.Morihara.config;
+
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public FilterRegistrationBean<LoginFilter> loginFilter() {
+        FilterRegistrationBean<LoginFilter> bean = new FilterRegistrationBean<>();
+
+        bean.setFilter(new LoginFilter());
+        //ログイン情報が必要なURL
+        bean.addUrlPatterns("/home");
+        bean.addUrlPatterns("/userEdit/*");
+        bean.addUrlPatterns("/singUp");
+        bean.setOrder(1);
+        return bean;
+    }
+}
